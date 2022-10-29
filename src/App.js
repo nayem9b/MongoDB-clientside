507570a6b1542d7e6a715b098b9f0ec0ad974e6b
nayem9b <razwanniam1@gmail.com>
2022-10-30 04:09:06 +0600

    user added sesction done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home";
+import AddUser from "./AddUser";
+import Update from "./Update";
 
 function App() {
-  const handleAddUser = () => {
-    console.log("hello");
-  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home></Home>,
+    },
+    {
+      path: "/users/add",
+      element: <AddUser></AddUser>,
+    },
+    {
+      path: "/update",
+      element: <Update></Update>,
+    },
+  ]);
   return (
     <div className='App'>
-      <form onSubmit={handleAddUser}>
-        <input type='text' name='name' placeholder='Name' />
-        <br />
-        <input type='email' name='email' id='' placeholder='Email' />
-        <br />
-        <button type='submit'>Add User</button>
-      </form>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
